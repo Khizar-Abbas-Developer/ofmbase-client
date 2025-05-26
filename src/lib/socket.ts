0206f4a3b7270a0d7058a5a37eb252ed3c2b6233
Khizar-Abbas-Developer <khizarnot7@gmail.com>
2025-05-26 22:27:35 +0500

    dfdfd

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,6 @@
 // socket.js
 import { io } from "socket.io-client";
-const socket = io("https://ofmbase-production.up.railway.app", {
+const socket = io("https://ofmbase-server-production.up.railway.app", {
   transports: ["websocket"], // force websocket
   withCredentials: true,
 });
