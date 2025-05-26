a698b24fc8084ebb2d47ec8ef2c44fdb2f5e1ebb
Khizar-Abbas-Developer <khizarnot7@gmail.com>
2025-05-26 22:02:30 +0500

    dfdf

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,5 +1,7 @@
 // socket.js
 import { io } from "socket.io-client";
-const socket = io("https://ofmbase-production.up.railway.app"); // Replace with your backend URL
-
+const socket = io("https://ofmbase-production.up.railway.app", {
+  transports: ["websocket"], // force websocket
+  withCredentials: true,
+});
 export default socket;
